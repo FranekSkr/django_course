209886e4e916cd05a7316d4dd5073307cdb61435
franek <franek.skrobot@gmail.com>
2022-10-16 14:03:23 +0200

    user registration

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
@@ -18,3 +20,28 @@ def counter(request):
         'words': len(words.split())
     }
     return render(request, 'counter.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'username already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'password do not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
